0c70953e4ae772eb182bba86696a7e667bfd03b5
Gianluca Bine <gian_bine@hotmail.com>
2024-06-03 19:10:32 -0300

    RefreshDatabase trait bug

---
+++ tests/Feature/ExampleOneTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class ExampleOneTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_refreshes_the_databases(): void
+    {
+        $this->assertNull(DB::connection('sqlite')->table('persons')->first());
+        $this->assertNull(DB::connection('sqlite_test')->table('persons')->first());
+    }
+}
